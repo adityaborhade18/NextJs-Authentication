3fe63661cb5c61ed6e5fa6685198bfc7d86aff65
Aditya-Borhade <adityaborhade793@gmail.com>
2025-07-31 23:29:39 +0530

    implement user signup with email verification

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,14 +1,30 @@
+import User from '@/models/userModel';
+import bcryptjs from 'bcryptjs';
 import nodemailer from 'nodemailer'
 
 
 export const sendEmail=async({email,emailType,userId}:any)=> {
   try {
+
+     const hashedToken=await bcryptjs.hash(userId.toString(),10);
+
+     if(emailType==="VERIFY"){
+       await User.findByIdAndUpdate(userId,
+         {verifyToken:hashedToken , verifyTokenExpiry:Date.now()+3600000 }
+       )
+     }else if(emailType==="RESET"){
+       await User.findByIdAndUpdate(userId,
+         {forgotPasswordToken:hashedToken ,  forgotPasswordTokenExpiry:Date.now()+3600000 }
+       )
+     }
+
+
     // Create transporter
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: 'yourgmail@gmail.com',
-        pass: 'your_app_password' // Use App Password, not your Gmail password
+        pass: 'your_app_password'
       }
     });
 
@@ -17,7 +33,8 @@ export const sendEmail=async({email,emailType,userId}:any)=> {
       from: 'yourgmail@gmail.com',
       to: email,
       subject: emailType==="VERIFY" ? "Verify your email" : "Reset your password",
-     
+      html:`<p>Click <a href='${process.env.DOMAIN}/verifyemail?token=${hashedToken}'>here</a> to ${ emailType==="VERIFY" ? "Verify your email" : "Reset your password"} or copy and paste the 
+      the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`
     };
 
     // Send email
